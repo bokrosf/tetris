5c6a737c91dd080fc2803410b78943122faec34d
bokrosf <bokrosf@owner.com>
2025-12-15 18:11:56 +0100

    Configuration asset and loading added.

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,21 @@
 #include <iostream>
+#include <exception>
 #include <SDL3/SDL_main.h>
+#include <configuration.h>
 
 int main(int argc, char *argv[])
 {
     std::cout << "Tetris" << std::endl;
+
+    try
+    {
+        load_configuration("asset/config.json");
+    }
+    catch (const std::exception &ex)
+    {
+        std::cout << ex.what() << std::endl;
+        throw;
+    }
 
     return 0;
 }
