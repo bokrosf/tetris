8628b576d42cc99537f5861aa1d8aeb52a18947d
bokrosf <bokrosf@owner.com>
2025-12-20 12:39:56 +0100

    label added.

---
+++ src/ui.h
@@ -0,0 +1,20 @@
+#ifndef TETRIS_UI_H
+#define TETRIS_UI_H
+
+#include <string>
+#include <SDL3/SDL.h>
+#include <asset.h>
+
+namespace ui
+{
+    struct label
+    {
+        std::string text;
+        asset::id_type font;
+        float font_size;
+        SDL_FColor color;
+        SDL_Point position;
+    };
+}
+
+#endif
