d86b7243bff865bff2caebb01d669574e9204fae
bokrosf <bokrosf@owner.com>
2025-12-21 19:23:23 +0100

    Tetromino definitions added.

--- src/game.h
+++ src/game.h
@@ -3,6 +3,18 @@
 
 namespace game
 {
+    using dimension = unsigned int;
+    const dimension part_dimension = 4;
+
+    struct tetromino
+    {
+        dimension width;
+        dimension height;
+        char parts[part_dimension][part_dimension];
+    };
+
+    extern tetromino piece_templates[7];
+
     struct game_state
     {
     };
