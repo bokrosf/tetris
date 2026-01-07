bcf393c1a4d49c4492139692f640e442bb72ae22
bokrosf <bokrosf@owner.com>
2026-01-07 12:13:39 +0100

    Texture loading added to asset.

--- src/asset.cpp
+++ src/asset.cpp
@@ -6,6 +6,7 @@
 #include <nlohmann/json.hpp>
 #include <asset.h>
 #include <font.h>
+#include <render.h>
 
 using json = nlohmann::ordered_json;
 
@@ -16,6 +17,7 @@ namespace
 
     id_type last_id = initial_id_value;
     std::unordered_map<id_type, ui::font> fonts;
+    std::unordered_map<id_type, SDL_Texture *> textures;
 }
 
 namespace asset
@@ -53,14 +55,53 @@ namespace asset
         return last_id;
     }
 
+    id_type load_texture(const std::string &path)
+    {
+        if (!std::filesystem::exists(path))
+        {
+            throw std::runtime_error(std::format("Texture asset not existing: {}", path));
+        }
+
+        SDL_Surface *surface = SDL_LoadBMP(path.c_str());
+
+        if (!surface)
+        {
+            throw std::runtime_error(std::format("Texture asset loading failed. Error: {}, path: {}", SDL_GetError(), path));
+        }
+
+        SDL_Texture *texture = render::load_texture(surface);
+
+        if (!texture)
+        {
+            throw std::runtime_error(std::format("Texture asset loading failed. Error: {}, path: {}", SDL_GetError(), path));
+        }
+
+        SDL_DestroySurface(surface);
+        ++last_id;
+        textures[last_id] = texture;
+
+        return last_id;
+    }
+
     void unload_all()
     {
         fonts.clear();
+
+        for (auto [id, t] : textures)
+        {
+            SDL_DestroyTexture(t);
+        }
+
         last_id = initial_id_value;
     }
 
     ui::font &font(id_type id)
     {
         return fonts.at(id);
+    }
+
+    SDL_Texture &texture(id_type id)
+    {
+        return *textures.at(id);
     }
 }
--- src/asset.h
+++ src/asset.h
@@ -2,6 +2,7 @@
 #define TETRIS_ASSET_H
 
 #include <string>
+#include <SDL3/SDL.h>
 #include <font.h>
 
 namespace asset
@@ -9,8 +10,10 @@ namespace asset
     using id_type = unsigned int;
 
     id_type load_font(const std::string &path);
+    id_type load_texture(const std::string &path);
     void unload_all();
     ui::font &font(id_type id);
+    SDL_Texture &texture(id_type id);
 }
 
 #endif
